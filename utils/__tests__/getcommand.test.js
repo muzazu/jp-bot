4130220a1a3023430892a9198ae8b496fe5b6d59
Muzazu <richardmmeans322@gmail.com>
2020-12-08 13:15:40 +0700

    update test

--- utils/__tests__/getcommand.test.js
+++ utils/__tests__/getcommand.test.js
@@ -1,18 +1,21 @@
 const mockMessage = require('../../__mocks__/message')
 const getcommand = require('../getcommand')
 
+let data = {}
+beforeEach(() => {
+	data = mockMessage
+})
 describe('getCommand', () => {
 	test('should return command name', () => {
-		console.log(mockMessage)
-		const cmd = getcommand(mockMessage)[0]
+		const cmd = getcommand(data)[0]
 		expect(cmd).toBe('kanji')
 	})
 	test('should return empty arg', () => {
-		const arg = getcommand(mockMessage)[1]
+		const arg = getcommand(data)[1]
 		expect(arg).toBe('')
 	})
 	test('should return empty filters', () => {
-		const arg = getcommand(mockMessage)[2]
+		const arg = getcommand(data)[2]
 		expect(arg).toMatchObject({})
 	})
 })
