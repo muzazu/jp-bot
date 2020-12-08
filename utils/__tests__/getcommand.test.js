731c3a7c3828c0b9c79eca61268d6d83993d3e5e
Muzazu <richardmmeans322@gmail.com>
2020-12-08 13:07:55 +0700

    test github action mock

--- utils/__tests__/getcommand.test.js
+++ utils/__tests__/getcommand.test.js
@@ -1,17 +1,18 @@
-const message = require('../../__mocks__/message')
+const mockMessage = require('../../__mocks__/message')
 const getcommand = require('../getcommand')
 
 describe('getCommand', () => {
 	test('should return command name', () => {
-		const cmd = getcommand(message)[0]
+		console.log(mockMessage)
+		const cmd = getcommand(mockMessage)[0]
 		expect(cmd).toBe('kanji')
 	})
 	test('should return empty arg', () => {
-		const arg = getcommand(message)[1]
+		const arg = getcommand(mockMessage)[1]
 		expect(arg).toBe('')
 	})
 	test('should return empty filters', () => {
-		const arg = getcommand(message)[2]
+		const arg = getcommand(mockMessage)[2]
 		expect(arg).toMatchObject({})
 	})
 })
